d3fb41f8143cd36de6049a9be00aa400f6084ef3
Toby Lopez <sudo.user.lopez@outlook.com>
2025-08-27 00:46:25 +0100

    automated script

--- .config/rofi/wallpapers.sh
+++ .config/rofi/wallpapers.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-wallpaper_directory="/home/toby/Pictures/wallpapers" # change the directory according to your preference
+# Directory containing wallpapers
+wall_dir="$HOME/Pictures/wallpapers"
 
-# all wallpapers
-wallpapers=$(ls $wallpaper_directory)
+# List files in the wallpaper directory and let rofi pick one
+chosen=$(ls "$wall_dir" | rofi -dmenu -i -p "Select wallpaper:" \
+  -theme "$HOME/.config/rofi/catppuccin-lavrent-mocha.rasi")
 
-# getting the current wallpaper with `swww query`
-current_wallpaper_path=$(swww query | sed "s/.*image: \(.*\)/\1/")
-current_wallpaper_name=${current_wallpaper_path:t}
+# If user cancels, exit
+[ -z "$chosen" ] && exit 0
 
-# adding " (current)" to the current wallpaper
-final_list=$(echo $wallpapers | sed "s/\($current_wallpaper_name\)/\1 (current)/")
-
-# displaying the menu
-selected_wallpaper=$(echo $final_list | rofi -dmenu) # add a theme to the rofi menu
-
-# removing the added " (current)" from the selected wallpaper (no matter the item selected)
-final_wallpaper=$(echo $selected_wallpaper | sed "s/ (current)//")
-
-# changing the wallpaper and the colorscheme if selected wallpaper is not empty
-if [[ $selected_wallpaper != "" ]]; then
-  swww img $wallpaper_directory/$final_wallpaper --transition-type fade --transition-fps 60 --transition-step 100 &&
-    wallust run $wallpaper_directory/$final_wallpaper -b kmeans -p dark16
-fi
+# Run swww with transition
+swww img "$wall_dir/$chosen" --transition-type wipe --transition-angle 30 --transition-step 90
