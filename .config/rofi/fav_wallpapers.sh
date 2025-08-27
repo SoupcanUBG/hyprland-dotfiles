4b2a1baeef0b286f9bf31589e03cd076a524f3bf
Toby Lopez <sudo.user.lopez@outlook.com>
2025-08-27 01:00:27 +0100

    automated script

---
+++ .config/rofi/fav_wallpapers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Directory containing wallpapers
+wall_dir="$HOME/Pictures/wallpapers/favourites/"
+
+# List files in the wallpaper directory and let rofi pick one
+chosen=$(ls "$wall_dir" | rofi -dmenu -i -p "Select wallpaper:" \
+  -theme "$HOME/.config/rofi/catppuccin-lavrent-mocha.rasi")
+
+# If user cancels, exit
+[ -z "$chosen" ] && exit 0
+
+# Run swww with transition
+swww img "$wall_dir/$chosen" --transition-type wipe --transition-angle 30 --transition-step 90
